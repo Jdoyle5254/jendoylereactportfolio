e69d302c6e13c0a427117d7636f6a4aaa7853834
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-02-28 17:33:08 -0600

    added images, set up main page container styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import React from "react"
 import NavBar from "./components/NavBar"
 import LinksMenu from "./components/LinksMenu"
 import Footer from "./components/Footer"
+import AboutMe from "./components/AboutMe"
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import {Container, Col, Row} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <NavBar />
-    <LinksMenu/>
-    <Footer />
+    <div className="container">
+      <NavBar />
+      <Container>
+        <Row>
+          <Col md="2">
+            <LinksMenu />
+          </Col>
+          <Col md="10">
+            <AboutMe />
+          </Col>
+
+        </Row>
+
+      </Container>
+
+      <Footer />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,44 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import App from "../App";
+import PortfolioPage from "./PortfolioPage"
+  
 
 export default  function NavBar() {
+  const date = new Date ()
+  const hours = date.getHours()
+  let timeOfDay
+    
+
+  if (hours <12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
     return (
+         
         <div className="navbar">
-        <h3 >navbar links here </h3>
-        <Link></Link>
-        <Link></Link>
-        <Link></Link>
+        <h3 >Good {timeOfDay} and welcome to my world! </h3>
+        <Router>
+            <Link to = "/">Home </Link>
+            <Link to="/portfoliopage">Portfolio</Link>
+
+            <Switch>
+            <Route exact path="/">
+                <App />
+            </Route>
+            <Route path="/portfolio">
+                <PortfolioPage/>
+            </Route>
+            </Switch>
+        </Router>
+        
         </div>
+
+        
+      
     )
 
 }
