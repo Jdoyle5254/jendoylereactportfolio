219b75b1cf37befb760092a17a0960f726406ae4
Jen Doyle <jdoyle5254@users.noreply.github.com>
2021-03-01 15:33:52 -0600

    updated projectsData file

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,43 +1,86 @@
+// {
+//     id: 1,
+//     image: "",
+//     projectTitle: "",
+//     github: "",
+//     deploy: "",
+//     tech: [""],
+//     description: "",
+// },
+
+
+
+
+
 export const projectsData = [
     {
         id: 1,
-        projimg: "",
+        // projimg: "./Images/Keep on Giftin'.png",
         title: "Keep on Giftin'",
         github: "https://github.com/girl-power-5/ubiquitous-giggle",
         deploy: "https://keep-on-giftin.herokuapp.com/",
-        tech: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "MySQL"],
+        tech: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "MySQL", "Bootstrap", "Bootswatch", "Heroku"],
         description: "An application for a user to create profiles to remind them to send a card or gift for special occasions and holidays"
     },
 
     {
         id: 2,
-        image: "",
-        projectTitle: "",
-        github: "",
-        deploy: "",
-        tech: [""],
-        description: "",
+        image: "./Images/NotesCreated.PNG",
+        projectTitle: "Note Taker",
+        github: "https://github.com/Jdoyle5254/note_taker_w11",
+        deploy: "https://wonderful-note-taker.herokuapp.com/notes",
+        tech: ["HTML", "CSS", "JavaScript", "Heroku", "Node.js"],
+        description: "An application for a user to create and save notes"
 },
-// {
-//     id: 1,
-//     image: "",
-//     projectTitle: "",
-//     github: "",
-//     deploy: "",
-//     tech: [""],
-//     description: "",
-// },
-// {
-//     id: 1,
-//     image: "",
-//     projectTitle: "",
-//     github: "",
-//     deploy: "",
-//     tech: [""],
-//     description: "",
-// },
+{
+    id: 3,
+    image: "./Images/PasswordGenerated.PNG",
+    projectTitle: "Password Generator",
+    github: "https://github.com/Jdoyle5254/passwordgenerator-w3",
+    deploy: "https://jdoyle5254.github.io/passwordgenerator-w3/",
+    tech: ["HTML", "CSS", "JavaScript"],
+    description: "An application that will generate a random password, based on user inputs"
+},
+{
+    id: 4,
+    image: "",
+    projectTitle: "JenDoyle Portfolio",
+    github: "https://jdoyle5254.github.io/JenDoyle-Portfolio/",
+    deploy: " https://jdoyle5254.github.io/JenDoyle-Portfolio/",
+    tech: ["HTML", "CSS", "Bootstrap"],
+    description: "Original portfolio in HTML and CSS ",
+},
 
+{
+    id: 5,
+    image: "./Images/Header.PNG",
+    projectTitle: "Weather Dashboard",
+    github: "https://github.com/Jdoyle5254/weather-dashboard-w6",
+    deploy: " https://jdoyle5254.github.io/weather-dashboard-w6/",
+    tech: ["HTML", "CSS", "Handlebars", "mySQL", "JavaScript"],
+    description: "An application that allows a user to pick a city and the app will display the current weather and the 5-day forecast for that city.  The app will store previous searches as a quick link." 
+    
+},
 
+{
+    id: 6,
+    image: "./Images/Page.PNG",
+    projectTitle: "Devour Da Burger",
+    github: " https://github.com/Jdoyle5254/burger_app_w13",
+    deploy: "https://devour-da-burger-app.herokuapp.com/",
+    tech: ["HTML", "CSS", "MySQL", "JavaScript", "Express", "Express-Handlebars", "Heroku"],
+    description: "A fun application that has a user input a burger and then devour it."
+},
+
+{
+    id: 6,
+    image: "./Images/readhere.png",
+    projectTitle: "Read Here",
+    github: "https://github.com/TheDreamTeam-NUBootCamp/ReadHere",
+    deploy: "https://thedreamteam-nubootcamp.github.io/ReadHere/",
+    tech: ["HTML", "CSS", "JavaScript", "API's", "JQuery", "Bootstrap"],
+    description: "An application created for a user who likes to read and travel.   Designed for a traveler who wants to find a book set in the city they are going to visit.   The User can input a city and a list of books set in that city is rendered"
+},
 
 
 ]
